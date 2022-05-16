91c510b13989118db2aba8593f311cd91f1bedfd
SonSonder <maceomaddox@gmail.com>
2022-05-16 14:28:50 -0400

    Began creating the variables for the passwords

--- script.js
+++ script.js
@@ -14,15 +14,39 @@ function writePassword() {
 }
 
 function generatePassword() {
+  var lowerChars = "abcdefghijklmnopqrstuvwxyz"
+  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var passwordLength = prompt ("What is the length of the password?")
   console.log (passwordLength)
   // Check if password length is between 8 and 128 characters
+  if (passwordLength < 8) {
+    alert ("Password too short")
+    return "Password too short"
+  }
   
+  if (passwordLength > 128) {
+    alert ("Password too long")
+    return "Password too long"
+  }
   // Ask user what kind of characters they want (lowercase, uppercase, numeric, special)
-  // There should be at least one character type selected
+  var hasLower = confirm ("Do you want lowercase characters?")
 
+  console.log (hasLower)
+  var chosenCharacters = ""
+  if (hasLower) {
+  chosenCharacters = chosenCharacters + lowerChars
+  }
+  
+  var hasUpper = confirm ("Do you want uppercase characters?")
+
+  console.log (hasUpper)
+  if (hasUpper) {
+    chosenCharacters = chosenCharacters + upperChars
+    }
+  // There should be at least one character type selected
+  var password = "example"
   // Create random password using aforementioned criteria
-  // return password
+  return password
 }
 
 // Add event listener to generate button
